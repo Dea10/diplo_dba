7190f6841974d3e38e1df96f59a8fc13f6a818b1
Daniel Espinosa <d111092.de@gmail.com>
2021-05-26 22:51:20 -0500

    p08 2/2

--- p08.sql
+++ p08.sql
@@ -56,16 +56,64 @@ WHERE TABLE_NAME='Productos';
 SHOW TABLE STATUS WHERE name='Productos';
 
 -- Create index
-
-IDProd
-Descripcion
-Unidades
-Costo
-IDDepto
-
 CREATE INDEX idx_0 ON Productos (IDProd);
 CREATE INDEX idx_1 ON Productos (Descripcion);
 CREATE INDEX idx_2 ON Productos (Unidades);
 CREATE INDEX idx_3 ON Productos (Costo);
 CREATE INDEX idx_4 ON Productos (IDDepto);
 CREATE INDEX idx_5 ON Productos ((IDDepto + Costo));
+
+-- 5 Emp(IDEmp, Nombre, Tel, Sueldo, FechaNac)
+CREATE TABLE Emp (
+    IDEmp INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(25) NOT NULL,
+    Tel VARCHAR(25) NOT NULL,
+    Sueldo DECIMAL(10,2) NOT NULL,
+    FechaNac DATE NOT NULL
+);
+
+INSERT INTO Emp(Nombre, Tel, Sueldo, FechaNac) VALUES('Daniel', '55 0000 0000', 0, '1992-11-10');
+
+-- UPDATE statement
+UPDATE Emp SET Sueldo = 1 WHERE IDEmp = 1;
+
+-- Create logs table
+CREATE TABLE logs (
+    id_log INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    event_type VARCHAR(25) NOT NULL,
+    user_name VARCHAR(25) NOT NULL,
+    user_ip VARCHAR(25) NOT NULL,
+    exec_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create on update trigger
+DELIMITER //
+CREATE TRIGGER update_sueldo AFTER UPDATE ON Emp
+FOR EACH ROW
+BEGIN  
+    DECLARE user_name VARCHAR(25);
+    DECLARE host_name VARCHAR(50);
+
+    SET @user_name = (SELECT USER());
+    SET @host_name = (SELECT @@hostname);
+
+    INSERT INTO logs(event_type, user_name, user_ip) VALUES('UPDATE on Sueldo', user_name, host_name);
+END;//
+DELIMITER ;
+
+DROP TRIGGER update_sueldo;
+
+-- Create on delete trigger
+DELIMITER //
+CREATE TRIGGER delete_emp AFTER DELETE ON Emp
+FOR EACH ROW
+BEGIN  
+    DECLARE user_name VARCHAR(25);
+    DECLARE host_name VARCHAR(50);
+
+    SET @user_name = (SELECT USER());
+    SET @host_name = (SELECT @@hostname);
+
+    INSERT INTO logs(event_type, user_name, user_ip) VALUES('DELETE on Emp', user_name, host_name);
+END;//
+DELIMITER ;
